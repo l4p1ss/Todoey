0546472574799e585e3a7371c883a0c89b1e5f69
lollopesci <lorenzo.pesci1@stud.unifi.it>
2020-01-08 12:11:47 +0100

    Bug fixes and performance improvements.

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
 //        print(Realm.Configuration.defaultConfiguration.fileURL)
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initialising new real, \(error)")
         }
